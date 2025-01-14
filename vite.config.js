c2cc59b188da8f6f9b0085188201196b2fcac3af
Antonio Kim <antoniok@sfu.ca>
2025-01-13 18:13:40 -0800

    adding testing library to vitest

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    global: true,
+    environment: 'jsdom',
+    setupFiles: ['src/vitest/registerMatchers.js'],
   },
 });
